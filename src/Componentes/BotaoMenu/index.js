beb5f909ea41159ae96b64cc9c4cabe9d0d239fd
José Carlos <jjc.silva10@gmail.com>
2021-05-27 16:54:54 -0300

    Criar Componente BotaoMenu

---
+++ src/Componentes/BotaoMenu/index.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import estilos from './index.module.css';
+
+const BotaoMenu = ({ menuAtivo, setMenuAtivo }) => {
+  const menuToggle = React.useCallback(() => {
+    setMenuAtivo((menuAtivo) => !menuAtivo);
+  }, [setMenuAtivo]);
+  
+  return (
+    <button
+      className={`${estilos.BotaoMenu} ${(menuAtivo) ? estilos.ativo : ''}`}
+      onClick={menuToggle}
+      aria-label={`${(menuAtivo) ? 'Fecha' : 'Abre'} o menu de navegação`}
+    > 
+      <span aria-hidden="true"></span>
+      <span aria-hidden="true"></span>
+      <span aria-hidden="true"></span>
+    </button>
+  )
+}
+
+export default BotaoMenu;
