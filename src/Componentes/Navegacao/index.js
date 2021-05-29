6aa839fd10a8bb17ec4058c723df8c4149b0be0b
Paulo <pauloh16.pdr@gmail.com>
2021-05-29 19:48:03 -0300

    Ajustar estilos de Navegacao

--- src/Componentes/Navegacao/index.js
+++ src/Componentes/Navegacao/index.js
@@ -1,21 +1,39 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom';
+import React from 'react';
 import estilos from './index.module.css';
+import { NavLink } from 'react-router-dom';
+import IconeHome from '../../Assets/iconeHome.svg';
+import IconeRegistros from '../../Assets/iconeRegistros.svg';
+import IconeHistoricos from '../../Assets/iconeHistoricos.svg';
 
 const Navegacao = () => {
     return (
         <nav className={estilos.Navegacao}>
             <ul>
                 <li>
-                    <NavLink to="/" activeClassName={estilos.ativo} end>Home</NavLink>
+                    <NavLink to="/" activeClassName={estilos.ativo} end>
+                        <div className={estilos.iconeContainer}>
+                            <img src={IconeHome} alt="Ícone da página Home." />
+                        </div>
+                        Home
+                    </NavLink>
                 </li>
 
                 <li>
-                    <NavLink to="/Registros" activeClassName={estilos.ativo} end>Registros</NavLink>
+                    <NavLink to="/Registros" activeClassName={estilos.ativo} end>
+                        <div className={estilos.iconeContainer}>
+                            <img src={IconeRegistros} alt="Ícone da página Home." />
+                        </div>
+                        Registros
+                    </NavLink>
                 </li>
 
                 <li>
-                    <NavLink to="/Historicos" activeClassName={estilos.ativo} end>Historicos</NavLink>
+                    <NavLink to="/Historicos" activeClassName={estilos.ativo} end>
+                        <div className={estilos.iconeContainer}>
+                            <img src={IconeHistoricos} alt="Ícone da página Home." />
+                        </div>
+                        Historicos
+                    </NavLink>
                 </li>
             </ul>
         </nav>
